845e7365fea9810d2d2734f7590a56a28fb56cd9
AcidRainFckU <matweev.ilja@yandex.ru>
2021-12-28 14:58:00 +0300

    Сделал урок №2

---
+++ src/tutorial-2/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  let email = "";
+  let password = "";
+  function changeInputs(event) {
+    event.target.type == "text"
+      ? (email = event.target.value.trim())
+      : (password = event.target.value.trim());
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (email === "" || password === "") {
+      alert("Поля не должны быть пустыми");
+    } else {
+      event.target.reset();
+      console.log({ email, password });
+      email = "";
+      password = "";
+    }
+  }
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={changeInputs} placeholder="Email" />
+        <input type="password" onChange={changeInputs} placeholder="Пароль" />
+        <button type="submit">Войти</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
